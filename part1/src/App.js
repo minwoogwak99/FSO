fe149cee973c0af51fd651cf57a0fe38a8daeb76
곽민우 <minwoogwak99@gmail.com>
2022-07-03 13:34:11 +0000

    Set up to the boiler template and did some test

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Hello = (props) => {
+	return (
+		<div>
+		<p>Hello from Hello Component!</p>
+		<p>Hi {props.name}, you are {props.age}</p>
+		</div>
+	)
 }
 
-export default App;
+const App = () => {
+	
+	const now = new Date()
+	const a = 10
+	const b = 20
+	const name = 'Minwoo'
+	const age = 24
+	
+	console.log('hello from component')
+
+	return (
+	<div>
+		<p>Hello world, it is {now.toString()}</p>
+		<p>{a} + {b} = {a+b}</p>
+		<Hello name={name} age={age}  />
+	</div>
+	)
+
+	
+	
+}
+
+export default App
