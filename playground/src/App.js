f9c03e023143c59b59144ef0f0c74a2efdd0dd98
Minwoo Gwak <mgwak2@illinois.edu>
2022-07-11 13:36:23 +0000

    Set up the project

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,33 +1,16 @@
-const Hello = (props) => {
+const App = (props) => {
+	const { notes } = props
+  
 	return (
-		<div>
-		<p>Hello from Hello Component!</p>
-		<p>Hi {props.name}, you are {props.age}</p>
-		</div>
+	  <div>
+		<h1>Notes</h1>
+		<ul>
+		  <li>{notes[0].content}</li>
+		  <li>{notes[1].content}</li>
+		  <li>{notes[2].content}</li>
+		</ul>
+	  </div>
 	)
-}
-
-const App = () => {
-	
-	const now = new Date()
-	const a = 10
-	const b = 20
-	const name = 'Minwoo'
-	const age = 24
-	
-	console.log('hello from component!!')
-	
-
-	return (
-	<div>
-		<p>Hello world, it is {now.toString()}</p>
-		<p>{a} + {b} = {a+b}</p>
-		<Hello name={name} age={age}  />
-	</div>
-	)
-
-	
-	
-}
-
-export default App
+  }
+  
+  export default App
